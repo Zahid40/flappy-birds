6509914acae3b0d504312c6d46ad6e679ae4240e
Techmier <72434947+Zahid40@users.noreply.github.com>
2025-04-09 01:17:12 +0530

    Setup with assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import '../global.css';
 
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <StatusBar />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,51 @@
-import { Stack, Link } from 'expo-router';
+import { Canvas, Image, useImage } from '@shopify/react-native-skia';
+import { useEffect } from 'react';
+import { useWindowDimensions } from 'react-native';
+import {
+  useSharedValue,
+  withTiming,
+  Easing,
+  withRepeat,
+  withSequence,
+} from 'react-native-reanimated';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+const App = () => {
+  const { width, height } = useWindowDimensions();
+  const bg = useImage(require('../assets/sprites/background-day.png'));
+  const bird = useImage(require('../assets/sprites/yellowbird-upflap.png'));
+  const pipeBottom = useImage(require('../assets/sprites/pipe-green-bottom.png'));
+  const pipeTop = useImage(require('../assets/sprites/pipe-green-top.png'));
+  const base = useImage(require('../assets/sprites/base.png'));
 
-export default function Home() {
+  const x = useSharedValue(width - 50);
+  const y = useSharedValue(0);
+
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(-200, { duration: 2000, easing: Easing.linear }),
+        withTiming(width, { duration: 0 })
+      ) , -1
+    );
+  }, []);
+
+  const pipeOffset = 50;
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <Canvas style={{ width, height }}>
+      <Image image={bg} width={width} height={height} fit={'cover'} />
+      <Image image={bird} width={64} height={48} x={123} y={height / 2} fit={'contain'} />
+      <Image image={pipeTop} width={104} height={640} x={x} y={pipeOffset - 320} fit={'contain'} />
+      <Image
+        image={pipeBottom}
+        width={104}
+        height={640}
+        x={x}
+        y={height - 320 + pipeOffset}
+        fit={'contain'}
+      />
+      <Image image={base} width={width} height={100} x={0} y={height - 75} fit={'cover'} />
+    </Canvas>
   );
-}
+};
+
+export default App;
